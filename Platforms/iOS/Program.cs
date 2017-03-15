b2e220f303bde2a22bb212002acf9cc09ccf512b
Simon Jackson <darkside@zenithmoon.com>
2017-03-15 19:06:08 +0000

    Initial Check-in

---
+++ Platforms/iOS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace GameStateManagementSample
+{
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate
+    {
+        private static GameStateManagementGame game;
+
+        internal static void RunGame()
+        {
+            game = new GameStateManagementGame();
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
